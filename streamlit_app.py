fb4f6940cbe07693101c22041b0aac8b56f9d592
PatchyLeopard <patchyleopard@gmail.com>
2025-06-30 16:00:58 +0200

    Update streamlit_app.py
    clean up code

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,19 +34,13 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
   
         search_on = pd_df.loc[pd_df.FRUIT_NAME == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.text(search_on + " Nuthaf*ka")
       
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    #st.write(ingredients_string)
-
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order+"""')"""
-
-    #st.write(my_insert_stmt)
-    #st.stop()
     
     time_to_insert = st.button('Submit Order')
 
